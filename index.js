fba0bd351bf9cc59a5884021fcf225715d30e4d3
MarcosFabSoftware2 <kauanplg2020@gmail.com>
2025-05-31 19:58:23 -0300

    Verificação se um usuário entrou em um canal de voz

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// index.js
+//Importando biblioteca dotenv
 require('dotenv').config();
+
+//Importando biblioteca do discord
 const { Client, GatewayIntentBits } = require('discord.js');
+
+//Importando id do canal
+const ID_CHANNEL = process.env.ID_CHANNEL;
 
 const client = new Client({
   intents: [
@@ -11,8 +16,25 @@ const client = new Client({
   ],
 });
 
+//Evento que verifica se o bot está pronto
 client.once('ready', () => {
   console.log(`🤖 Bot conectado como ${client.user.tag}`);
 });
 
+//Evento que verifica atualizações em canais de voz
+    client.on('voiceStateUpdate', (oldState, newState) => {
+        
+        //Verificação se um usuário entrou em um canal de voz
+        if(!oldState.channel && newState.channel) {
+            console.log(`${newState.member.user.tag} entrou no canal de voz ${newState.channel.name}`);
+        }
+
+        //Verificação se um usuário saiu de um canal de voz
+        else if(oldState.channel && !newState.channel) {
+            console.log(`${oldState.member.user.tag} saiu do canal de voz ${oldState.channel.name}`);
+        }
+    })
+    
+
+//Conectando ao bot discord
 client.login(process.env.DISCORD_TOKEN);
