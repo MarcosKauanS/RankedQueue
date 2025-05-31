38360187c70d373285215f6f53bf9ca9b0354761
MarcosFabSoftware2 <kauanplg2020@gmail.com>
2025-05-31 18:41:27 -0300

    Configurações iniciais

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`🤖 Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
