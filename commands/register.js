115df19bf76f3c56fa61d466ac2a423bc9129812
MarcosFabSoftware2 <kauanplg2020@gmail.com>
2025-08-18 17:51:41 -0300

    feat(bot): adicionar comando /registrar com registro no MySQL e prevenção de duplicidade

--- commands/register.js
+++ commands/register.js
@@ -4,39 +4,42 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('registrar')
         .setDescription("Registra você para acessar os canais exclusivos"),
-    async execute(interaction) {
+    async execute(interaction, db) {
         const REGISTER_CHANNEL_ID = process.env.REGISTER_CHANNEL_ID;
         const REGISTER_ROLE_ID = process.env.REGISTER_ROLE_ID;
 
         if(interaction.channel.id !== REGISTER_CHANNEL_ID) return;
-        
-        if(interaction.content == '!registrar') {
             try {
-            const role = interaction.guild.roles.cache.get(REGISTER_ROLE_ID);
-            const username = interaction.member.user.username;
-            const elo = 0;
-            const newUsername = `[${elo}]${username}`;
+                const role = interaction.guild.roles.cache.get(REGISTER_ROLE_ID);
+                const username = interaction.member.user.username;
+                const elo = 0;
+                const newUsername = `[${elo}]${username}`;
+                const userId = interaction.member.id;
 
-            if(interaction.member.id == interaction.guild.ownerId) {
-                return interaction.reply("Não posso modificar o apelido do dono do servidor")
-            }
+                if(interaction.member.id == interaction.guild.ownerId) {
+                    return interaction.reply("Não posso modificar o apelido do dono do servidor")
+                }
 
-            if(!role) {
-                return interaction.reply("Cargo de registro não encontrado");
-            
-            } else {
-                await interaction.member.roles.add(role);
+                const existingUser = await db.getUser(userId);
 
-                await interaction.member.setNickname(newUsername);
+                if(existingUser) {
+                    return interaction.reply("Você já está registrado!")
+                }
 
-                await interaction.reply("Você foi registrado");
-            }
+                if(!role) {
+                    return interaction.reply("Cargo de registro não encontrado");
+                } else {
+                    await interaction.member.roles.add(role);
+
+                    await interaction.member.setNickname(newUsername);
+
+                    await db.addUser(userId, username, elo);
+
+                    await interaction.reply("Você foi registrado");
+                }
             }catch(error) {
-            console.error(error);
-            interaction.reply("Ocorreu um erro ao tentar registrar você")
+                console.error(error);
+                interaction.reply("Ocorreu um erro ao tentar registrar você")
             }
-        }
-
-        await interaction.reply("Você usou o comando de registro");
     },
 };
