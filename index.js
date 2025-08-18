b0a0a1bb9401fde32f25a0af0305fbcbeaf1fd92
MarcosFabSoftware2 <kauanplg2020@gmail.com>
2025-08-18 17:51:54 -0300

    .

---
+++ index.js
@@ -0,0 +1,140 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection, ChannelType, Events} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const Database = require('./database/Database');
+const db = new Database();
+
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const GUILD_ID = process.env.GUILD_ID
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  if('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  }
+}
+
+client.once('ready', () => {
+  console.log(`🤖 Bot conectado como ${client.user.tag}`);
+});
+
+let memberCall = 0;
+let gameNumber = 0
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+
+  
+  if(!oldState.channel && newState.channel) {
+    console.log(`${newState.member.user.tag} entrou no canal de voz ${newState.channel.name}`);
+    memberCall += 1;
+    console.log(memberCall);
+  }
+
+  else if(oldState.channel && !newState.channel) {
+    console.log(`${oldState.member.user.tag} saiu do canal de voz ${oldState.channel.name}`);
+    if(memberCall < 0 ) {
+      memberCall = 0;
+      console.log(memberCall);
+    }else {
+     memberCall -= 1;
+     console.log(memberCall);
+    }
+  }
+
+  if(memberCall == 2) {
+    const guild = client.guilds.cache.get(GUILD_ID);
+    const voiceChannel = guild.channels.cache.get(CHANNEL_ID);
+
+    const membersInCall = Array.from(voiceChannel.members.values());
+
+    for (let i = membersInCall.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [membersInCall[i], membersInCall[j]] = [membersInCall[j], membersInCall[i]];
+    }
+
+    const middleIndex = Math.ceil(membersInCall.length / 2);
+    const team1 = membersInCall.slice(0, middleIndex);
+    const team2 = membersInCall.slice(middleIndex);
+
+    const team1Mentions = team1.map(member => `<@${member.id}>`).join(' ');
+    const team2Mentions = team2.map(member => `<@${member.id}>`).join(' ');
+
+    gameNumber += 1;
+
+    guild.channels.create( {
+      name: `JOGO #${gameNumber}`, 
+      type: ChannelType.GuildText
+    }).then(channel => {
+      channel.send(`${team1Mentions}`);
+      channel.send(`${team2Mentions}`);
+    });
+
+    guild.channels.create({
+      name: `JOGO #${gameNumber} [Time 1]`,
+      type: ChannelType.GuildVoice
+    });
+
+    guild.channels.create({
+      name: `JOGO #${gameNumber} [Time 2]`,
+      type: ChannelType.GuildVoice
+    });
+
+    memberCall = 0;
+  }
+});
+
+client.on('interactionCreate', async interaction => {
+  const command = client.commands.get(interaction.commandName);
+  
+  await command.execute(interaction, db);
+});
+
+(async () => {
+  await db.connect();
+  await db.createUsersTable();
+
+  // Registro dos comandos Slash
+  const { REST, Routes } = require('discord.js');
+  const CLIENT_ID = process.env.CLIENT_ID;
+
+  const commands = [];
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    }
+  }
+
+  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+  try {
+    console.log('Atualizando comandos Slash...');
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    );
+    console.log('Comandos Slash atualizados!');
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Login do bot
+  client.login(process.env.DISCORD_TOKEN);
+})();
